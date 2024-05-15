20180469e55917e44ddce368343154ce7ee7f776
Krzysiaczek <133698272+Krzysiaczek420@users.noreply.github.com>
2024-05-15 19:33:12 +0200

    Update LoginPage.py

--- LoginPage.py
+++ LoginPage.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import RegisterPage
 import Baza as baza
 import PinWindow as pin
+import ForgetPasswordWindow as forgetPW
 
 #=========================================================================
 #           główne okno strony logowania
@@ -30,11 +31,16 @@ def NewUserButtonPressing():
     login_window.withdraw()
     RegisterPage.open_register_window(login_window)
 
+
+def forget_password_pressed():
+    login_window.withdraw()
+    forgetPW.select_username(login_window, run_LoginPage)
+
 def on_closing():
     if login_window:
         try:
             baza.close_database_connection()
-            login_window.destroy()
+            login_window.after(100, login_window.destroy)
         except tk.TclError:
             pass
 
@@ -46,14 +52,49 @@ def run_LoginPage():
 #=========================================================================
 # Widgety
 #=========================================================================
-login_input = ctk.CTkEntry(login_window, width=150, height=30, placeholder_text="login")
-password_input = ctk.CTkEntry(login_window, width=150, height=30, placeholder_text="password", show ="*")
-show_password_cb = ctk.CTkCheckBox(login_window, text="show password", width=150, height=30, command=show_password)
-login_button = ctk.CTkButton(login_window, text="Log in", width=60, height=30)
-newUserButton = ctk.CTkButton(login_window, text="New account", width=60, height=30, command=NewUserButtonPressing)
-newAccountLabel = ctk.CTkLabel(login_window, text="New user? Create account", width=140, height=20)
-loginLabel = ctk.CTkLabel(login_window, text="logowanie", width=140, height=20)
-forgetPassword = ctk.CTkButton(login_window, text="Forget password", width=60, height=30)
+login_input = ctk.CTkEntry(login_window, 
+    width=150, 
+    height=30, 
+    placeholder_text="login")
+
+password_input = ctk.CTkEntry(login_window, 
+    width=150, 
+    height=30, 
+    placeholder_text="password", 
+    show ="*")
+
+show_password_cb = ctk.CTkCheckBox(login_window, 
+    text="show password", 
+    width=150, 
+    height=30, 
+    command=show_password)
+
+login_button = ctk.CTkButton(login_window, 
+    text="Log in", 
+    width=60, 
+    height=30)
+
+newUserButton = ctk.CTkButton(login_window, 
+    text="New account", 
+    width=60, 
+    height=30, 
+    command=NewUserButtonPressing)
+
+newAccountLabel = ctk.CTkLabel(login_window, 
+    text="New user? Create account", 
+    width=140, 
+    height=20)
+
+loginLabel = ctk.CTkLabel(login_window, 
+    text="logowanie", 
+    width=140, 
+    height=20)
+
+forgetPassword = ctk.CTkButton(login_window, 
+    text="Forget password", 
+    width=60, 
+    height=30,
+    command=forget_password_pressed)
 
 #=========================================================================
 # Umieszczenie widgetów
@@ -71,3 +112,4 @@ newUserButton.place(x=180, y=460)
 # Run
 #=========================================================================
 run_LoginPage()
+#potrzebuje rownież aby zamkniecie ktoregokolwiek okna w ForgetPasswordWindow i wcisniecie guzika submit w change_password w ForgetPasswordWindow spowrotem otwierało okno loginPage
