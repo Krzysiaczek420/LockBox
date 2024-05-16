9491feb7ad48740397d9d5c4095586b1b65ffb5e
Krzysiaczek <133698272+Krzysiaczek420@users.noreply.github.com>
2024-05-16 15:21:50 +0200

    Create Motyw.py

---
+++ Motyw.py
@@ -0,0 +1,28 @@
+import json
+
+def wczytaj_ustawienia(sciezka):
+    with open(sciezka, 'r') as plik:
+        ustawienia = json.load(plik)
+    return ustawienia
+
+def ustaw_motyw_i_kolor(sciezka_do_pliku):
+    # Słownik mapujący nazwy kolorów na ich ścieżki do plików JSON lub nazwy domyślne
+    kolory = {
+        "blue": "motywy/blue.json",
+        #"dark-blue": "dark-blue",
+        "green": "motywy/green.json",
+        "red": "motywy/red.json",
+        "yellow": "motywy/yellow.json",
+        "orange": "motywy/orange.json",
+        "pink": "motywy/pink.json",
+        "violet": "motywy/violet.json"
+    }
+    
+    ustawienia = wczytaj_ustawienia(sciezka_do_pliku)
+    kolor = ustawienia.get('color', 'default_color')
+    motyw = ustawienia.get('theme', 'default_theme')
+    
+    # Pobierz odpowiednią wartość ze słownika kolory
+    kolor = kolory.get(kolor, kolor)  # Jeśli kolor nie jest w słowniku, użyj go bezpośrednio
+
+    return motyw, kolor
