5e902c1ce4ae1de1a22791c07b0e9938432ce29a
Krzysiaczek <133698272+Krzysiaczek420@users.noreply.github.com>
2024-05-17 02:09:46 +0200

    Create Szyfrowanie.py

---
+++ Szyfrowanie.py
@@ -0,0 +1,32 @@
+import hashlib
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import base64
+
+# Funkcja haszująca hasło użytkownika za pomocą SHA-256
+def hash_password(password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    return hashed_password
+
+# Funkcja generująca klucz z hasła użytkownika i pinu
+def generate_key(answer, pin, salt):
+    combined = answer + pin
+    key = hashlib.sha256(combined.encode()).digest()
+    return key
+
+# Funkcja szyfrująca hasło serwisu
+def encrypt_service_password(service_password, key):
+    cipher = AES.new(key, AES.MODE_GCM)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(service_password.encode('utf-8'))
+    return base64.b64encode(nonce + tag + ciphertext).decode('utf-8')
+
+# Funkcja deszyfrująca hasło serwisu
+def decrypt_service_password(enc_service_password, key):
+    enc_service_password = base64.b64decode(enc_service_password)
+    nonce = enc_service_password[:16]
+    tag = enc_service_password[16:32]
+    ciphertext = enc_service_password[32:]
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    service_password = cipher.decrypt_and_verify(ciphertext, tag)
+    return service_password.decode('utf-8')
