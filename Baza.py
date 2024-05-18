a46eac5ab8ead8cbc0625d62cd43d85b3f9ade3c
Krzysiaczek <133698272+Krzysiaczek420@users.noreply.github.com>
2024-05-18 20:49:10 +0200

    Update Baza.py

--- Baza.py
+++ Baza.py
@@ -3,14 +3,18 @@ import pymongo
 from Crypto.Random import get_random_bytes
 import base64
 import Szyfrowanie
-
+################################################################
+#       dane bazy danych
+################################################################
 DATABASE_URL = "mongodb://localhost:27017/"
 DATABASE_NAME = "LockBox"
 USERS_COLLECTION_NAME = "users"
 
 client = None
 users_collection = None
-
+################################################################
+# funkcja do laczenia z baza danych
+################################################################
 def connect_to_database():
     global client, users_collection
     client = pymongo.MongoClient(DATABASE_URL)
@@ -18,24 +22,28 @@ def connect_to_database():
     users_collection = database[USERS_COLLECTION_NAME]
     print("Połączenie z bazą danych zostało otwarte.")
 
+
+################################################################
+# funkcja do zamkniecia polaczenia z baza danych
+#################################################################
 def close_database_connection():
     if client:
         client.close()
         print("Połączenie z bazą danych zostało zamknięte.")
 
+#################################################################
+# dodawanie nowego uzytkownika (rejestracja)
+#################################################################
 def add_user(username, password, security_question, answer, pin_code):
     existing_user = users_collection.find_one({"username": username})
     if existing_user:
-        return {"success": False, "message": "Użytkownik o tej nazwie już istnieje."}
+        return {"success": False, "message": "Username already exist."}
     
-    # Haszowanie hasła użytkownika
     hashed_password = Szyfrowanie.hash_password(password)
-    
-    # Generowanie losowego klucza symetrycznego
+
     key = get_random_bytes(32)
     salt = get_random_bytes(16)
-    
-    # Szyfrowanie klucza za pomocą hasła użytkownika i pinu
+
     key_encryption_key = Szyfrowanie.generate_key(answer, pin_code, salt)
     encrypted_key = Szyfrowanie.encrypt_service_password(base64.b64encode(key).decode('utf-8'), key_encryption_key)
 
@@ -50,17 +58,18 @@ def add_user(username, password, security_question, answer, pin_code):
         "systems": []
     }
     users_collection.insert_one(user_data)
-    return {"success": True, "message": "Użytkownik został pomyślnie dodany."}
+    return {"success": True, "message": "User has been added succesfully."}
 
-def change_user_password(username, new_password):
+#################################################################
+# reset hasla uzytkownika(forget password)
+#################################################################
+def reset_user_password(username, new_password):
     user = users_collection.find_one({"username": username})
     if not user:
-        return {"success": False, "message": "Użytkownik nie istnieje."}
+        return {"success": False, "message": "User does not exist."}
 
-    # Generowanie nowego hasła
     new_hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
 
-    # Aktualizacja użytkownika w bazie danych
     users_collection.update_one(
         {"username": username},
         {"$set": {
@@ -68,36 +77,4 @@ def change_user_password(username, new_password):
         }}
     )
 
-    return {"success": True, "message": "Hasło zostało zmienione."}
-def reset_user_password(username, new_password, security_answer, pin_code):
-    user = users_collection.find_one({"username": username})
-    if not user:
-        return {"success": False, "message": "Użytkownik nie istnieje."}
-
-    # Weryfikacja odpowiedzi na pytanie bezpieczeństwa
-    if user["answer"] != security_answer:
-        return {"success": False, "message": "Niepoprawna odpowiedź na pytanie bezpieczeństwa."}
-
-    # Weryfikacja kodu pin
-    if user["pin_code"] != pin_code:
-        return {"success": False, "message": "Niepoprawny kod pin."}
-
-    # Odszyfrowanie starego klucza szyfrującego
-    salt = base64.b64decode(user["salt"])
-    old_key_encryption_key = Szyfrowanie.generate_key(user["answer"], pin_code, salt)
-    old_encrypted_key = base64.b64decode(user["encrypted_key"])
-    old_key = base64.b64decode(Szyfrowanie.decrypt_service_password(old_encrypted_key, old_key_encryption_key))
-
-    # Haszowanie nowego hasła użytkownika
-    new_hashed_password = Szyfrowanie.hash_password(new_password)
-
-    # Aktualizacja użytkownika w bazie danych
-    users_collection.update_one(
-        {"username": username},
-        {"$set": {
-            "password": new_hashed_password,
-            # Nie zmieniamy klucza, pozostawiamy go taki sam
-        }}
-    )
-
-    return {"success": True, "message": "Hasło zostało zresetowane."}
+    return {"success": True, "message": "Password has been changed succesfully."}
